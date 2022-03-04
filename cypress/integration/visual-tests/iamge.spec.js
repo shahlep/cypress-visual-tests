adc9f87f9a8a4237a58a050091588648764a9faf
Shah rahman <toshah.rahman@gmail.com>
2022-03-04 18:29:57 +0200

    single element snapshot

--- cypress/integration/visual-tests/iamge.spec.js
+++ cypress/integration/visual-tests/iamge.spec.js
@@ -2,5 +2,9 @@ describe('Visual Regression Tests', () => {
   it('visit website and compre', () => {
       cy.visit('https://example.com/')
       cy.matchImageSnapshot()
-  })  
+  })
+  it('should match a single elements', () => {
+    cy.visit('https://example.com/')
+    cy.get('h1').matchImageSnapshot()
+  })
 })
